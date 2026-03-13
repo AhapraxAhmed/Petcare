559b4025d03018b6961feff699ae467a3c5e4526
AHMED <aptech356@gmail.com>
2026-03-14 04:12:18 +0500

    delete extra files and push

--- check_app_data.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once "config/config.php";
-require_once "config/database.php";
-$db = Database::getInstance();
-$conn = $db->getConnection();
-$res = $conn->query("SELECT * FROM appointments LIMIT 1");
-print_r($res->fetch_assoc());
-?>
--- check_vet.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once "config/config.php";
-require_once "config/database.php";
-$db = Database::getInstance();
-$conn = $db->getConnection();
-$res = $conn->query("SHOW TABLES LIKE 'veterinarians'");
-echo "Veterinarians table exists: " . ($res->num_rows > 0 ? "YES" : "NO") . "\n";
-?>
